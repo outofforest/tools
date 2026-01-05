afde1eea079d5e4c8d857d49c8f67a5322a1d718
Wojciech Malota-Wojcik <wojtek@exw.co>
2026-01-05 09:34:52 +0100

    Fix zig urls and checksums

--- pkg/tools/zig/tools.go
+++ pkg/tools/zig/tools.go
@@ -17,24 +17,24 @@ var t = []tools.Tool{
 		Version: "0.15.2",
 		Sources: tools.Sources{
 			tools.PlatformLinuxAMD64: {
-				URL:  "https://ziglang.org/download/0.15.2/zig-linux-x86_64-0.15.2.tar.xz",
-				Hash: "sha256:d45312e61ebcc48032b77bc4cf7fd6915c11fa16e4aad116b66c9468211230ea",
+				URL:  "https://ziglang.org/download/0.15.2/zig-x86_64-linux-0.15.2.tar.xz",
+				Hash: "sha256:02aa270f183da276e5b5920b1dac44a63f1a49e55050ebde3aecc9eb82f93239",
 				Links: map[string]string{
-					"bin/zig": "zig-linux-x86_64-0.15.2/zig",
+					"bin/zig": "zig-x86_64-linux-0.15.2/zig",
 				},
 			},
 			tools.PlatformDarwinAMD64: {
-				URL:  "https://ziglang.org/download/0.15.2/zig-macos-x86_64-0.15.2.tar.xz",
-				Hash: "sha256:8b06ed1091b2269b700b3b07f8e3be3b833000841bae5aa6a09b1a8b4773effd",
+				URL:  "https://ziglang.org/download/0.15.2/zig-x86_64-macos-0.15.2.tar.xz",
+				Hash: "sha256:375b6909fc1495d16fc2c7db9538f707456bfc3373b14ee83fdd3e22b3d43f7f",
 				Links: map[string]string{
-					"bin/zig": "zig-macos-x86_64-0.15.2/zig",
+					"bin/zig": "zig-x86_64-macos-0.15.2/zig",
 				},
 			},
 			tools.PlatformDarwinARM64: {
-				URL:  "https://ziglang.org/download/0.15.2/zig-macos-aarch64-0.15.2.tar.xz",
-				Hash: "sha256:46fae219656545dfaf4dce12fb4e8685cec5b51d721beee9389ab4194d43394c",
+				URL:  "https://ziglang.org/download/0.15.2/zig-aarch64-macos-0.15.2.tar.xz",
+				Hash: "sha256:3cc2bab367e185cdfb27501c4b30b1b0653c28d9f73df8dc91488e66ece5fa6b",
 				Links: map[string]string{
-					"bin/zig": "zig-macos-aarch64-0.15.2/zig",
+					"bin/zig": "zig-aarch64-macos-0.15.2/zig",
 				},
 			},
 		},
